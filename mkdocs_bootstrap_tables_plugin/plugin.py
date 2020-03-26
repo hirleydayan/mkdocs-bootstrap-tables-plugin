f072d4163a1c6e00c11b39042d3120e4a7e4f07a
Hirley Dayan <hirleydayan@gmail.com>
2020-03-26 11:11:28 -0300

    Update plugin.py
    Fixing string_type issue

--- mkdocs_bootstrap_tables_plugin/plugin.py
+++ mkdocs_bootstrap_tables_plugin/plugin.py
@@ -4,14 +4,13 @@ import re
 from timeit import default_timer as timer
 from datetime import datetime, timedelta
 
-from mkdocs import utils as mkdocs_utils
 from mkdocs.config import config_options, Config
 from mkdocs.plugins import BasePlugin
 
 class BootstrapTablesPlugin(BasePlugin):
 
     config_scheme = (
-        ('param', config_options.Type(mkdocs_utils.string_types, default='')),
+        ('param', config_options.Type(str, default='')),
     )
 
     def __init__(self):
